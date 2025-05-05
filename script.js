66d427b50f98275919606fdcb7bd8698e487e4ef
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-05-05 12:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 128, 0)'; // Green
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 0, 0)'; // Black
+  });
+}
+   //Write your code here
 
     
 }
